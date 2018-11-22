17e907ee976599781c430ddcaa845bc3bbd0a7fa
Laurenzi <laurirutanen@gmail.com>
2018-11-22 18:39:02 +0200

    Part 3 excercises 3.1 - 3.6 done!

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+        {
+          "name": "Martti Tienari",
+          "number": "040-123456",
+          "id": 2
+        },
+        {
+          "name": "Arto Järvinen",
+          "number": "040-123456",
+          "id": 3
+        },
+        {
+          "name": "Lea Kutvonen",
+          "number": "040-123456",
+          "id": 4
+        },
+        {
+          "name": "Tervepä Terve",
+          "number": "123123",
+          "id": 6
+        }
+      ]
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post("/api/persons", (request, response) => {
+    const id = Math.floor(Math.random() * Math.floor(60000));
+    console.log(request.body)
+    const body = request.body
+    
+    if (body.name === undefined) {
+        return response.status(400).json({error: 'name missing'})
+    } else if (body.number === undefined) {
+        return response.status(400).json({error: 'name missing'})
+    } else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({error: 'name must be unique'})
+    }
+    const personObject = {
+        name: body.name,
+        number: body.number || '',
+        id: id
+    }
+    
+    persons = persons.concat(personObject)
+    response.json(personObject)
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        persons = persons.filter(person => person.id !== id)
+        response.status(204).end()
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get("/info", (request, response) => {
+    console.log("saatiin pyyntö!")
+    response.send(`<p>Puhelinluettelossa ${persons.reduce((acc, curr) => acc + 1, 0)} henkilön tiedot.</p>
+    <p>${new Date()}<p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
